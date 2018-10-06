a1cf5a1b073a6f3cabdbfae161e5515efe791125
micsebas <mic.sebastian@hotmail.com>
2018-10-07 00:32:35 +0800

    trial

--- Pipsqueak_Bot.py
+++ Pipsqueak_Bot.py
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler('start', start))
 
     updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook('https://capitalist-manifesto-bot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://pipsqueak-sutd-bot.herokuapp.com/' + TOKEN)
     updater.idle()
