8fb30930dfb649605274d0bc11fed5c3932e9b0c
micsebas <mic.sebastian@hotmail.com>
2018-10-07 00:59:55 +0800

    add message handler

--- Pipsqueak_Bot.py
+++ Pipsqueak_Bot.py
@@ -1,6 +1,5 @@
 import telegram
 import psycopg2
-import pytz
 import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 
@@ -11,18 +10,61 @@ BOT = telegram.Bot(token=TOKEN)
 BOT.setWebhook(url='https://pipsqueak-sutd-bot.herokuapp.com/' + TOKEN)
 
 
+class Database(object):
+
+    def __init__(self):
+        os.environ['DATABASE_URL'] = DATABASE_URL
+        self.conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+        self.cur = self.conn.cursor()
+        stmt = "CREATE TABLE IF NOT EXISTS ps_database (user_id BIGINT NOT NULL, name TEXT NOT NULL, state TEXT NOT NULL)"
+        self.cur.execute(stmt)
+        self.conn.commit()
+
+    def add_new_user(self, user_id, name):
+        stmt = "INSERT INTO ps_database (user_id, name, state) VALUES (%d, '%s', 'home')" % (user_id, name)
+        self.cur.execute(stmt)
+        self.conn.commit()
+
+    def get_users(self):
+        stmt = "SELECT user_id FROM ps_database"
+        self.cur.execute(stmt)
+        rows = self.cur.fetchall()
+        return [x[0] for x in rows]
+
+
 def start(bot, update):
+    global db
     user_id = update.message.from_user.id
+    users_list = db.get_users()
+    if user_id not in users_list:
+        name = update.message.from_user.first_name + ' ' + update.message.from_user.last_name
+        db.add_new_user(user_id, name)
     msg = 'Hello, world!'
     bot.send_message(user_id, msg)
 
 
-if __name__ == '__main__':
+def message_handler(bot, update):
+    admin_id = 111914928
+    sender_id = update.message.from_user.id
+    sender_name = update.message.from_user.first_name + ' ' + update.message.from_user.last_name
+    text = update.message.text
+    msg = '%s (%d) said "%s"' % (sender_name, sender_id, text)
+    bot.send_message(admin_id, msg)
+
+
+def main():
     updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
 
+    dispatcher.add_handler(MessageHandler(filters.Filters.text, message_handler))
+
     updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
     updater.bot.setWebhook('https://pipsqueak-sutd-bot.herokuapp.com/' + TOKEN)
     updater.idle()
+
+
+if __name__ == '__main__':
+    db = Database()
+    main()
