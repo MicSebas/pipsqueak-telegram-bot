c1d56237134a1e05248e0ded3a46b503dbb27bb6
micsebas <mic.sebastian@hotmail.com>
2018-10-09 01:17:07 +0800

    debug edit_message reply markup

--- Pipsqueak_Bot2.py
+++ Pipsqueak_Bot2.py
@@ -127,9 +127,10 @@ def callback_query_handler(bot, update):
         else:
             db.update_state(user_id, 'sell_Others')
             msg = 'You requested to sell an item which we may not prepared to host.\n\nBefore proceeding, please note that your request may be moderated and subject to approval. Do you want to continue?'
-            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Yes', callback_data=True), InlineKeyboardButton('No', callback_data=False)]])
-            # new_msg = bot.edit_message_text(msg, user_id, msg_id, reply_markup=None)
-            bot.edit_message_reply_markup(user_id, msg_id, reply_markup=keyboard)
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Yes', callback_data=True)],
+                                             [InlineKeyboardButton('No', callback_data=False)]])
+            bot.edit_message_text(msg, user_id, msg_id, reply_markup=keyboard)
+            # bot.edit_message_reply_markup(user_id, msg_id, reply_markup=keyboard)
     elif state == 'sell_Others':
         if data:
             db.update_state(user_id, 'sell_Others_request')
