b0cf74af873fbb3f3596496949ce5db9148df924
micsebas <mic.sebastian@hotmail.com>
2018-10-09 09:59:25 +0800

    debug buy

--- Pipsqueak_Bot2.py
+++ Pipsqueak_Bot2.py
@@ -264,7 +264,7 @@ def callback_query_handler(bot, update):
         else:
             item = db.get_items_dict(item_id=data)
             msg = 'You want to buy %s: %s for $%.2f.\n\nIs this correct?' % (item['name'], item['description'], item['price'])
-            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Yes', callback_data='True'), InlineKeyboardButton('No', callback_data=False)]])
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Yes', callback_data='True'), InlineKeyboardButton('No', callback_data='False')]])
             db.update_state(user_id, 'buy_item_%s' % data)
             bot.edit_message_text(msg, user_id, msg_id, reply_markup=keyboard)
     elif state.startswith('buy_item_'):
