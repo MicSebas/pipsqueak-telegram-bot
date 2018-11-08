35566293deebef94a17dec6412351920734d5cbb
MicSebas <mic.sebastian@hotmail.com>
2018-11-08 18:17:19 +0800

    test

--- Database.py
+++ Database.py
@@ -358,7 +358,7 @@ if __name__ == '__main__':
     items = db.get_requests()
     for item in items:
         print(item)
-    items = db.get_feedback()
+    items = db.get_activities()
     for item in items:
         print(item)
     print(len(users))
