bda4e30c079e4c41032c3df7563b0bfea77f46a4
MicSebas <mic.sebastian@hotmail.com>
2018-11-27 20:27:00 +0800

    debug

--- Pipsqueak_Bot3.py
+++ Pipsqueak_Bot3.py
@@ -1894,7 +1894,7 @@ def tompang_confirm(bot, update, state):
         msg_id = update.callback_query.message.message_id
         store = state.split('_')[1]
         db.update_state(user_id, 'tompang_%s_item' % store)
-        msg = 'Please send me the link for the item you want from %d.' % data
+        msg = 'Please send me the link for the item you want from %s.' % data
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('<< back', callback_data='back'),
                                           InlineKeyboardButton('/cancel', callback_data='cancel')]])
         bot.edit_message_text(msg, user_id, msg_id, reply_markup=keyboard)
